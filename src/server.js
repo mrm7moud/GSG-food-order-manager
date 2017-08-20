87569feb5e3bde383412d4326517e22c0d272a24
mhd <mhd@Hanis-MacBook-Pro.local>
2017-08-21 00:21:57 +0300

    create all server file

---
+++ src/server.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const router = require('./router.js');
+
+const port = process.env.PORT || 3000;
+const host = process.env.HOSR || 'localhost';
+
+const server = http.createServer(router);
+server.listen(port, () => {
+    console.log(`Server is up and running on : http://${host}:${port}`);
+});
