a912432d41de5e21f3064bf824e704c01fe36a3c
mrm7moud <mrm7moud@Hanis-MacBook-Pro.local>
2017-08-26 12:55:35 +0300

    edit url

--- src/router.js
+++ src/router.js
@@ -1,18 +1,18 @@
 const handler = require('./handler.js');
 module.exports = (req, res) => {
-    var endpoint = req.url;
-    var extension = endpoint.split('.')[1];
+  var endpoint = req.url;
+  var extension = endpoint.split('.')[1];
 
-    if (endpoint === '/') {
-        handler.serveHome(req, res);
-    } else if (extension === 'html' ||
-     extension === 'css' ||
-     extension === 'js' ||
-     extension === 'ico' ||
-     extension === 'png' ||
-     extension === 'jpg') {
-         handler.servePublic(req, res);
-    } else {
-        handler.serveError(req, res);
-    }
+  if (endpoint === '/') {
+    handler.serveHome(req, res);
+  } else if (extension === 'html' ||
+    extension === 'css' ||
+    extension === 'js' ||
+    extension === 'ico' ||
+    extension === 'png' ||
+    extension === 'jpg') {
+    handler.servePublic(req, res);
+  } else {
+    handler.serveError(req, res);
+  }
 };
